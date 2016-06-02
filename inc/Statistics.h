bffd9d698beedefd43cba898739769e37f10d6c7
Erik Zapata <zapatabrandon@gmail.com>
2016-06-02 09:37:01 -0400

    Starting to write functions to calculate deviations and such

---
+++ inc/Statistics.h
@@ -0,0 +1,11 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+double deviation(byte* color, byte* intArr, byte* avgColors, int size);
+
+
+#endif
